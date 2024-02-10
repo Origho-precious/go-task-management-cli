206dc4824324f33b051a39a7fdd31711584023dc
Origho-precious <Origho9@gmail.com>
2024-02-10 23:03:13 +0100

    feat: adding sql database connection

---
+++ configs/database.go
@@ -0,0 +1,39 @@
+package configs
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DbName   string
+	SSLMode  string
+}
+
+func (cfg PostgresConfig) String() string {
+	return fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DbName, cfg.SSLMode,
+	)
+}
+
+func ConnectDB() (*sql.DB, error) {
+	cfg := PostgresConfig{
+		Host:     "localhost",
+		Port:     "5432",
+		User:     "pilolo",
+		Password: "sredev",
+		DbName:   "task-management-cli",
+		SSLMode:  "disable",
+	}
+
+	db, err := sql.Open("pgx", cfg.String())
+
+	return db, err
+}
